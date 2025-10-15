2ebc8f6bc15e502fe1f83fdc9b52bdd8c1c7cb9b
Erick-Sptech <erick.ferreira@sptech.school>
2025-10-15 12:41:15 -0300

    adicionando o arquivo CSV ao projeto / Criação da função que lê e exibe o arquivo CSV

--- etl_autobotics/src/main/java/school/sptech/Main.java
+++ etl_autobotics/src/main/java/school/sptech/Main.java
@@ -1,17 +1,89 @@
 package school.sptech;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        exibeCsv("dados_hardware");
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void exibeCsv(String nomeArq) {
+        FileReader arq = null;
+        Scanner entrada = null;
+        Boolean erroGravar = false;
+        nomeArq += ".csv";
+
+        try {
+            arq = new FileReader(nomeArq);
+
+            entrada = new Scanner(arq).useDelimiter(",|\\n");
+        }
+        catch (FileNotFoundException erro) {
+            System.out.println("Arquivo inexistente!");
+            System.exit(1);
+        }
+
+        try {
+            Boolean cabecalho = true;
+            while (entrada.hasNextLine()) {
+                // le a linha inteira do csv
+                String linha = entrada.nextLine();
+                // divide essa linha em vários campos usando a virgula
+                // campos = 0 - timestamp, 1 - cpu, 2 - ramTotal, 3 - ramUsada, 4 - discoTotal, 5 - discoUsado
+                // 6 - numProcessos, 7 - codigoMaquina, 8 - top5Processos (sendo tratado como uma String inteira)
+
+                String[] campos = linha.split(",", 9);
+
+                if (cabecalho) {
+
+                    for (int i = 0; i < campos.length; i++) {
+                        campos[i] = campos[i].replaceAll("[^a-zA-Z0-9]", "");
+                    }
+                    System.out.printf("%-25s %-10s %-10s %-10s %-10s %-10s %-15s %-15s %-10s\n",
+                            campos[0], campos[1], campos[2], campos[3], campos[4], campos[5], campos[6], campos[7], campos[8]);
+                    cabecalho = false;
+                } else {
+                    String timestamp = campos[0].replaceAll("\"", "");
+                    Double cpu = Double.parseDouble(campos[1]);
+                    Double ramTotal = Double.parseDouble(campos[2]);
+                    Double ramUsada = Double.parseDouble(campos[3]);
+                    Double discoTotal = Double.parseDouble(campos[4]);
+                    Double discoUsado = Double.parseDouble(campos[5]);
+                    Integer numProcessos = Integer.parseInt(campos[6]);
+                    String codigoMaquina = campos[7].replaceAll("\"", "");
+                    String top5Processos = campos[8];  // JSON inteiro, sem alteração
+
+                    System.out.printf("%-25s %-10.2f %-10.2f %-10.2f %-10.2f %-10.2f %-15d %-15s %-10s\n",
+                            timestamp, cpu, ramTotal, ramUsada, discoTotal, discoUsado, numProcessos, codigoMaquina, top5Processos);
+                }
+            }
+        }
+        catch (NoSuchElementException erro) {
+            System.out.println("Arquivo com problemas!");
+            erro.printStackTrace();
+            erroGravar = true;
+        }
+        catch (IllegalStateException erro) {
+            System.out.println("Erro na leitura do arquivo!");
+            erro.printStackTrace();
+            erroGravar = true;
+        }
+        finally {
+            try {
+                entrada.close();
+                arq.close();
+            }
+            catch (IOException erro) {
+                System.out.println("Erro ao fechar o arquivo");
+                erroGravar = true;
+            }
+            if (erroGravar) {
+                System.exit(1);
+            }
         }
     }
 }
