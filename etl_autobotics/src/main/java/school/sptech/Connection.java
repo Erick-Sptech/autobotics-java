39ecbb2597054dc577e5cbb1b11a47005e680d4b
Joao-error404 <joao.oliveiraneto@sptech.school>
2025-10-28 09:06:37 -0300

    novo jar

--- etl_autobotics/src/main/java/school/sptech/Connection.java
+++ etl_autobotics/src/main/java/school/sptech/Connection.java
@@ -9,7 +9,7 @@ public class Connection {
         dataSource = new BasicDataSource();
 
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://54.152.61.170:3306/autobotics");
+        dataSource.setUrl("jdbc:mysql://52.2.40.203:3306/autobotics");
         dataSource.setUsername("agente");
         dataSource.setPassword("sptech");
 
